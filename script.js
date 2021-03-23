a198d401db946a209acefd3c4b5862e3c51dffa7
Vlad <7frativlad@gmail.com>
2021-03-23 10:49:38 +0200

    Schimbat si adaugat

---
+++ script.js
@@ -0,0 +1,9 @@
+let str= "This is normal string";
+let number = 42;
+let num = number*2;
+let booleann = false;
+let array = [1,2,3,4,5];
+let _undefined = undefined;
+let _null = null;
+let _NaN = NaN;
+console.log(num);
